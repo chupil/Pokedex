376e59644eec8ebb8ea80042684e6ece87e26d41
chupil <chupil87@gmail.com>
2020-04-23 19:27:09 +0300

    Updates

---
+++ precache-manifest.e08113bea38399982fefa70617a4dea7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e701b4c859fbce01f82a05f4b870b222",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "80827594c86af5eaa975",
+    "url": "/pokedex/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "9d4ed2ea09713336554e",
+    "url": "/pokedex/static/css/main.baef08f1.chunk.css"
+  },
+  {
+    "revision": "80827594c86af5eaa975",
+    "url": "/pokedex/static/js/2.34f77f0f.chunk.js"
+  },
+  {
+    "revision": "86493497955f2c03ad4534f44ae20baa",
+    "url": "/pokedex/static/js/2.34f77f0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d4ed2ea09713336554e",
+    "url": "/pokedex/static/js/main.0849b5a3.chunk.js"
+  },
+  {
+    "revision": "601f8d113175821311bd",
+    "url": "/pokedex/static/js/runtime-main.0ec6b405.js"
+  }
+]);
