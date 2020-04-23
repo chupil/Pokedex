13317d13a0069712fc00ed4570fef824e5aa1241
chupil <chupil87@gmail.com>
2020-04-23 19:14:58 +0300

    last

--- src/components/pokedex.js
+++ src/components/pokedex.js
@@ -1,4 +1,9 @@
+ 
+
+
 import React, { Component, Fragment } from "react";
+import $ from 'jquery';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 
 
@@ -6,24 +11,53 @@ export default class Pokedex extends Component {
   constructor(props) {
     super(props);
     this.listPokemonRender = this.listPokemonRender.bind(this);
-
-    this.onClickPokemonRender = this.onClickPokemonRender.bind(this);
-  
+    this.onClickPokemonRender = this.onClickPokemonRender.bind(this);  
     this.nextPokemonRender =   this.nextPokemonRender.bind(this);
+    this.menuPokemonRender =   this.menuPokemonRender.bind(this);
+    this.onClickGetType = this.onClickGetType.bind(this);
     this.state = {
       next: "https://pokeapi.co/api/v2/pokemon?limit=12",
       previous: null,
       lists: [],
       sLoaded :false,
       listsPokemonLoad:[],
-      pokemonRender : null
+      pokemonRender : null,      
+      typeList : [] 
+      
+      
+      
        
     };
   }
+  onClickGetType(e,type){   
+    
+    var typeSel = type;
+    var selectedType = [];
+    this.state.listsPokemonLoad.map((listsPokemonLoad,index)=>{
+      listsPokemonLoad.types.map((typeO,index)=>{                      
+        if (typeO.type.name == typeSel.name){
+
+          console.log(listsPokemonLoad);
+          Array.prototype.push.apply(selectedType, [listsPokemonLoad]);
+
+         
+          
+
+        }
+    })
+  })
+  if(selectedType.length > 0 ){
+    this.setState({listsPokemonLoad: selectedType  })
+  }
+this.forceUpdate();
+}
+
+  menuPokemonRender(e){
+    $().dropdown('show');
+}
 
   nextPokemonRender(){
-    this.setState({listsPokemonLoad : [],pokemonRender :[]});
-   
+    this.setState({listsPokemonLoad : [],pokemonRender :[], typeList:[]});   
     this.componentDidMount();
   }
 
@@ -70,10 +104,8 @@ export default class Pokedex extends Component {
                   <div className="col-4 div"> {listsPokemonLoad.moves.length}</div>
              </div>
 
-             <p className="card-text">stats</p>
-             <a href="#" className="btn btn-primary">
-               Go somewhere
-             </a>
+            
+            
            </div>
          </div>
        );
@@ -84,38 +116,34 @@ export default class Pokedex extends Component {
   }
 
 
-  listPokemonRender() {
-   
-
+  listPokemonRender() { 
+        
     return ( 
    this.state.listsPokemonLoad.map((listsPokemonLoad,index)=>{
+     
         return ( <div className="col-md-4" key={index}  onClick= {(e)=>{ this.onClickPokemonRender(e,listsPokemonLoad); }} >
         <div className="card div mt-3 mb-3" >
                   <img src={listsPokemonLoad.sprites.front_default} className="card-img-top div" alt="..." />
                  <div className="card-body">
-                    <h5 className="card-title text-center">{listsPokemonLoad.name.toUpperCase()}</h5>
-                    
+                    <h5 className="card-title text-center">{listsPokemonLoad.name.toUpperCase()}</h5>                    
                     <div className="row">
-                   {listsPokemonLoad.types.map((type,index)=>{
-
+                   {listsPokemonLoad.types.map((type,index)=>{                      
+                
                     return ( <div className="col-6 p-1"> 
-                         <a href="#" className="btn btn-primary w-100 "> {type.type.name}</a> 
-                        
-                      </div>)
-                      }
-                      )
-                      
+                         <a href="#" className={"btn btn-primary w-100  text-capitalize " 
+                         + type.type.name}> {type.type.name}</a> 
+                      </div>)})                     
                       }                     
                     </div>
                   </div>
                 </div>
                 </div>
-                )})
-    )
+                )})                
+                )
 
+                 
   }
-  componentDidMount() {
-   
+  componentDidMount() { 
 
     fetch(this.state.next)
       .then((res) => res.json())
@@ -136,7 +164,21 @@ export default class Pokedex extends Component {
               (result) => {
                 this.setState ({
                   listsPokemonLoad: [...this.state.listsPokemonLoad, result]
-                })              
+                })  
+                
+                let set =  new Set(); 
+                this.state.listsPokemonLoad.map((listsPokemonLoad,index)=>{
+                  listsPokemonLoad.types.map((type,index)=>{                      
+                    set.add(type.type.name);})
+                })  
+                var typeList = []  
+                for (let value of set){
+                var obj = [{ name : value}];  
+                Array.prototype.push.apply(typeList, obj); 
+               }
+               this.setState ({typeList : typeList});
+               
+
                 },              
               (error) => {
                 this.setState({
@@ -167,34 +209,53 @@ export default class Pokedex extends Component {
 
  
     return (
-
       <div className="container">
-        <div className="row justify-content-center ">
-          <div className="pl-5 pr-5 div w-50 text-center">
-            <h1>Pokedex</h1>{" "}
-          </div>
+        <div className="row justify-content-center ">  
+        <div class="dropdown w-50">
+    <button
+      class="btn btn-secondary dropdown-toggle  w-100"
+      type="button"
+      id="dropdownMenuButton"
+      data-toggle="dropdown"
+      aria-haspopup="true"
+      aria-expanded="false"
+     
+      onClick={(e)=> {  this.menuPokemonRender(e, "click") }}    >
+      <h1>Pokedex</h1>
+    </button>   
+
+     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">     
+     {this.state.typeList.map((type, index) => { 
+    return( <Fragment>
+   <a class="dropdown-item text-capitalize" href="#" onClick={(e)=> {  this.onClickGetType(e, type) }}>{type.name}</a>
+       </Fragment>
+    )
+})}    
+    </div>    
+      
+      </div>
+          
         </div>
         <div className="row">
           <div className="col-md-8">
             <div className="row">
-              
-              <Fragment>
-                {this.listPokemonRender()}
-              </Fragment>       
-              
+              <Fragment>{this.listPokemonRender()}</Fragment>
             </div>
-            <div className="row justify-content-center">
-              <a href="#" className="btn btn-primary w-75" onClick= {(e)=>{ this.nextPokemonRender(e); }}>
+            <div className="row justify-content-center mb-3">
+              <a
+                href="#"
+                className="btn btn-primary w-75 h-75"
+                onClick={(e) => {
+                  this.nextPokemonRender(e);
+                }}
+              >
                 Load More
               </a>
             </div>
           </div>
           <div className="col-md-4 d-flex align-items-center">
             {" "}
-            
-            <Fragment>
-                { this.state.pokemonRender}
-              </Fragment> 
+            <Fragment>{this.state.pokemonRender}</Fragment>
           </div>
         </div>
       </div>
